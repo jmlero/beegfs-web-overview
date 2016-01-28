9721c5ad367ae2a3280354bc340d1278f4005c8c
jmlero <jmlero@hotmail.com>
2016-01-28 16:43:44 +0100

    Add config file and try except

--- sqlite2elastic/sqlite2elastic.py
+++ sqlite2elastic/sqlite2elastic.py
@@ -28,58 +28,79 @@
 
 """
 Notes:
--
+- Read the last line of the admon database of a beegfs file system
+- Extract the performance metrics of the metadata and storage servers
+- Post the metrics to elastic server
 TODO list:
--
+- Read options from a config file
 """
 
 # imports
+import sys
 import sqlite3
 import elasticsearch
 from datetime import datetime
 import requests
-# import argparse
+import ConfigParser
+import logging
+import argparse
 # import os
-# import tarfile
-# import shutil
 # import json
 # import errno
 # import string
 
 
 # global variables
-_SCRIPT_VERSION = '0.2'
+_SCRIPT_VERSION = '0.3'
 
-_ELASTIC_SERVER = 'http://elastic.int.cemm.at:9200'
-_ELASTIC_SERVER_PORT = 9200
+_CFG_FILE = r'/root/sqlite2elastic.cfg'
+# _ELASTIC_SERVER_DIR = "elastic.int.cemm.at"
+# _ELASTIC_SERVER_PORT = "9200"
 _BEEGFS_DB = "/mnt/fhgfsMgmt/admon/fhgfs-admon-data.db"
 
+# Main function
+def main():
+    """
+    Parameters:
+    Returns:
+    Raises:
+        ValueError for invalid arguments
+    """
+    # get args
+    # args = parseargs()
 
-res = requests.get(_ELASTIC_SERVER)
-con = sqlite3.connect(_BEEGFS_DB)
-es = elasticsearch.Elasticsearch([{'host': 'elastic.int.cemm.at', 'port': _ELASTIC_SERVER_PORT}])
+    config = ConfigParser.RawConfigParser()
+    try:
+        config.read(_CFG_FILE)
+    except Exception as ex:
+        logging.exception("Config file " + _CFG_FILE + " not found")
+        sys.exit()
+        
+    try:
+        req = requests.get("http://" + config.get('elastic', 'address') + ":" + config.get('elastic', 'port'))
+    except Exception as ex:
+        logging.exception("Elastic server " + config.get('elastic', 'address') + ":" + config.get('elastic', 'port') + " not reachable")
+        sys.exit()
 
-print(res.content)
+    print(req.content)
 
-row_meta = con.execute("select is_responding, workRequests, queuedRequests from metaNormal order by time desc limit 0,1")
-row_storage = con.execute("select is_responding, diskRead, diskWrite, diskReadPerSec, diskWritePerSec, diskSpaceTotal, diskSpaceFree from storageNormal order by time desc limit 0,1")
+    req.close()
 
-for row in row_meta:
-        print row[0]
-        print row[1]
-        print row[2]
+    con_db = sqlite3.connect(config.get('beegfs', 'db'))
+
+    try:
+        row_meta = con_db.execute("select is_responding, workRequests, queuedRequests from metaNormal order by time desc limit 0,1")
+        row_storage = con_db.execute("select is_responding, diskRead, diskWrite, diskReadPerSec, diskWritePerSec, diskSpaceTotal, diskSpaceFree from storageNormal order by time desc limit 0,1")
+    except Exception as ex:
+        logging.exception("Database " + config.get('beegfs', 'db') + " not found")
+        sys.exit()
+
+    for row in row_meta:
         meta_is_responding = row[0]
         meta_workRequests = row[1]
         meta_queuedRequests = row[2]
 
-for row in row_storage:
-        print row[0]
-        print row[1]
-        print row[2]
-        print row[3]
-        print row[4]
-        print row[5]
-        print row[6]
+    for row in row_storage:
         storage_is_responding = row[0]
         storage_diskRead = row[1]
         storage_diskWrite = row[2]
@@ -88,30 +109,33 @@ for row in row_storage:
         storage_diskSpaceTotal = row[5]
         storage_diskSpaceFree = row[6]
 
-meta_body = {
-    "timestamp": datetime.now(),
-    'is_responding': meta_is_responding,
-    'workRequests': meta_workRequests,
-    'queuedRequests': meta_queuedRequests
-}
+    con_db.close()
 
-storage_body = {
-    "timestamp": datetime.now(),
-    "is_responding": storage_is_responding,
-    "diskRead": storage_diskRead,
-    "diskWrite": storage_diskWrite,
-    "diskReadPerSec": storage_diskReadPerSec,
-    "diskWritePerSec": storage_diskWritePerSec,
-    "diskSpaceTotal": storage_diskSpaceTotal,
-    "diskSpaceFree": storage_diskSpaceFree
-}
+    meta_body = {
+        "timestamp": datetime.now(),
+        "is_responding": meta_is_responding,
+        "workRequests": meta_workRequests,
+        "queuedRequests": meta_queuedRequests
+    }
 
-print datetime.now()
+    storage_body = {
+        "timestamp": datetime.now(),
+        "is_responding": storage_is_responding,
+        "diskRead": storage_diskRead,
+        "diskWrite": storage_diskWrite,
+        "diskReadPerSec": storage_diskReadPerSec,
+        "diskWritePerSec": storage_diskWritePerSec,
+        "diskSpaceTotal": storage_diskSpaceTotal,
+        "diskSpaceFree": storage_diskSpaceFree
+    }
 
-res_meta = es.index(index="beegfs-data-ms01", doc_type="metrics-meta",  body=meta_body)
+    es = elasticsearch.Elasticsearch([{'host': "elastic.int.cemm.at", 'port': 9200}])
 
-res_storage = es.index(index="beegfs-data-storage01", doc_type="metrics-storage", body=storage_body)
+    res_meta = es.index(index="beegfs-data-ms01", doc_type="metrics-meta",  body=meta_body)
+
+    res_storage = es.index(index="beegfs-data-storage01", doc_type="metrics-storage", body=storage_body)
 
 
-res.close()
-con.close()
+# MAIN
+if __name__ == '__main__':
+    main()
